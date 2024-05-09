bc539691ec1755aa0c8eadaa5860600a5d36e49f
14987 <nirajgautam196@gmail.com>
2024-05-09 18:13:15 +0530

    refactor: Add util fun & remove debug statement

--- timesheet/src/lib/ProtectedRoute.tsx
+++ timesheet/src/lib/ProtectedRoute.tsx
@@ -6,13 +6,11 @@ export const ProtectedRoute = () => {
   const navigate = useNavigate();
   const { currentUser, isLoading } = useContext(UserContext);
   if (isLoading) {
-    console.log("Loading----"); // TODO: Add a loading spinner
+    // TODO: Add a loading spinner
   } else if (!currentUser || currentUser === "Guest") {
-    console.log("Redirecting to login");
     navigate("/login?redirect-to=/timesheet");
     window.location.reload();
     return null;
   }
-  console.log(currentUser);
   return <Outlet />;
 };
--- timesheet/src/lib/utils.ts
+++ timesheet/src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(";").shift();
+}
