fab7a380d160c74530882e09adaefc0c8881f039
14987 <nirajgautam196@gmail.com>
2024-05-13 19:11:36 +0530

    fix: Update proxy options

--- timesheet/vite.config.ts
+++ timesheet/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
+import proxyOptions from "./proxyOptions";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: proxyOptions,
+  },
   build: {
     outDir: "../timesheet_enhancer/public/timesheet",
     emptyOutDir: true,
