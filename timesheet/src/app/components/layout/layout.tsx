07fcf19a305b7950e443c6c0746bfed4e6d6c947
14987 <nirajgautam196@gmail.com>
2024-05-24 15:10:02 +0530

    fix: load employee in useEffect

--- timesheet/src/app/components/layout/layout.tsx
+++ timesheet/src/app/components/layout/layout.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Header } from "@/app/components/layout/Header";
+import { Toaster } from "@/components/ui/toaster";
+import { setEmployee } from "@/app/state/employee";
 import { AppDispatch } from "@/app/state/store";
-import {  useDispatch } from "react-redux";
-import { fetchEmployee } from "@/app/state/employee";
-import { Toaster } from "@/components/ui/toaster"
+import { useDispatch } from "react-redux";
+import { RootState } from "@/app/state/store";
+import { useSelector } from "react-redux";
+import { FrappeConfig, FrappeContext } from "frappe-react-sdk";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const employee = useSelector((state: RootState) => state.employee);
   const dispatch = useDispatch<AppDispatch>();
-  dispatch(fetchEmployee());
+  const { call } = useContext(FrappeContext) as FrappeConfig;
+  useEffect(() => {
+    (async () => {
+      call
+        .get("timesheet_enhancer.api.utils.get_employee_from_user")
+        .then((res) => {
+          dispatch(setEmployee(res?.message));
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    })();
+  }, []);
+
   return (
     <div>
       <Header />
       <div className="lg:max-w-screen-lg lg:mr-auto lg:ml-auto md:pr-5 md:pl-5 pl-2 pr-3 xl:pr-0 xl:pl-0 pt-4">
-        {children}
+        {employee.value && children}
       </div>
       <Toaster />
     </div>
