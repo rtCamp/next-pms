eb869b8ec54aab65204f27a50384664de76a0da8
14987 <nirajgautam196@gmail.com>
2024-06-10 18:35:59 +0530

    feat: Add Icon component

--- timesheet/src/components/ui/button.tsx
+++ timesheet/src/components/ui/button.tsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        gradient: "text-primary-foreground bg-gradient-to-r from-gradientPrimary to-gradientSecondary",
+        outlineSecondary: "border border-gradientPrimary text-gradientPrimary bg-background hover:text-gradientPrimary"
       },
       size: {
         default: "h-10 px-4 py-2",
