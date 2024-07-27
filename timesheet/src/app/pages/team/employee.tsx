34084f1712cc681d98bd48cb2f57bc53e15383a8
14987 <nirajgautam196@gmail.com>
2024-07-27 19:08:24 +0530

    feat: Add team view page
    Added load more, filters and week updates.

---
+++ timesheet/src/app/pages/team/employee.tsx
@@ -0,0 +1,212 @@
+import { RootState } from "@/store";
+import { useSelector } from "react-redux";
+import { useFrappeGetCall } from "frappe-react-sdk";
+import { Spinner } from "@/app/components/spinner";
+import { Table, TableBody, TableCell, TableRow } from "@/app/components/ui/table";
+import { Typography } from "@/app/components/typography";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/app/components/ui/tooltip";
+import { cn, floatToTime, getDateFromDateAndTime } from "@/lib/utils";
+import { PencilLine, CirclePlus } from "lucide-react";
+import { useState } from "react";
+import { TaskDataProps, TaskDataItemProps, LeaveProps } from "@/types/timesheet";
+
+interface EmployeeProps {
+  employee: string;
+}
+
+export const Employee = ({ employee }: EmployeeProps) => {
+  const teamState = useSelector((state: RootState) => state.team);
+  const { data, isLoading } = useFrappeGetCall("timesheet_enhancer.api.team.get_timesheet_for_employee", {
+    employee: employee,
+    date: teamState.weekDate,
+  });
+  if (isLoading) {
+    return <Spinner />;
+  }
+  const timesheetData = data?.message[Object.keys(data?.message)[0]];
+
+  return (
+    <div>
+      <Table>
+        <TableBody>
+          {timesheetData.leaves.length > 0 && <LeaveRow dates={timesheetData.dates} leaves={timesheetData.leaves} />}
+          {Object.keys(timesheetData.tasks).length == 0 && (
+            <EmptyRow dates={timesheetData.dates} holidays={timesheetData.holidays} />
+          )}
+          {Object.keys(timesheetData.tasks).length > 0 &&
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            //   @ts-ignore
+            Object.entries(timesheetData.tasks).map(([task, taskData]: [string, TaskDataProps]) => {
+              let totalHours = 0;
+              return (
+                <TableRow key={task} className="border-b border-slate-200 flex">
+                  <TableCell className="max-w-md w-full">
+                    <Typography variant="p" className="text-slate-800">
+                      {task}
+                    </Typography>
+                    <Typography variant="small" className="text-slate-500">
+                      {taskData.project_name}
+                    </Typography>
+                  </TableCell>
+                  {timesheetData.dates.map((date: string) => {
+                    const data = taskData.data.find(
+                      (data: TaskDataItemProps) => getDateFromDateAndTime(data.from_time) === date
+                    );
+                    if (data && data.hours) {
+                      totalHours += data.hours;
+                    }
+                    const isHoliday = timesheetData.holidays.includes(date);
+                    return <Cell date={date} data={data} isHoliday={isHoliday} />;
+                  })}
+                  <TableCell>
+                    <Typography variant="p" className="text-slate-800 font-medium">
+                      {floatToTime(totalHours)}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export const EmptyRow = ({
+  dates,
+  holidays,
+  onCellClick,
+}: {
+  dates: string[];
+  holidays: string[];
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  onCellClick?: (data) => void;
+}) => {
+  return (
+    <TableRow className="flex">
+      <TableCell className="w-full max-w-md">
+        <Typography variant="p" className="text-destructive">
+          No Task found
+        </Typography>
+      </TableCell>
+      {dates.map((date: string) => {
+        const isHoliday = holidays.includes(date);
+        const value = {
+          date,
+          hours: "",
+          description: "",
+          isUpdate: false,
+          name: "",
+          parent: "",
+          task: "",
+        };
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //   @ts-ignore
+        return <Cell date={date} data={value} isHoliday={isHoliday} onCellClick={onCellClick} />;
+      })}
+      <TableCell className="w-full max-w-24 text-left"></TableCell>
+    </TableRow>
+  );
+};
+
+const Cell = ({
+  date,
+  data,
+  isHoliday,
+  onCellClick,
+  disabled,
+}: {
+  date: string;
+  data: TaskDataItemProps | undefined;
+  isHoliday: boolean;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  onCellClick?: (val) => void;
+  disabled?: boolean;
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const isDisabled = disabled || data?.docstatus === 1;
+  const handleClick = () => {
+    if (isDisabled || !data) return;
+    const value = {
+      date: date,
+      hours: data?.hours ?? "",
+      description: data?.description ?? "",
+      isUpdate: data?.hours > 0 ? true : false,
+      name: data?.name ?? "",
+      parent: data?.parent ?? "",
+      task: data?.task ?? "",
+    };
+
+    onCellClick && onCellClick(value);
+  };
+  const onMouseEnter: React.MouseEventHandler<HTMLTableCellElement> = () => {
+    if (isDisabled) return;
+    setIsHovered(true);
+  };
+  const onMouseLeave: React.MouseEventHandler<HTMLTableCellElement> = () => {
+    if (isDisabled) return;
+    setIsHovered(false);
+  };
+  return (
+    <Tooltip >
+      <TableCell
+        key={date}
+        onMouseEnter={onMouseEnter}
+        onClick={handleClick}
+        onMouseLeave={onMouseLeave}
+        className={cn(
+          "max-w-20 w-full text-left",
+          isDisabled && "cursor-default",
+          isHovered && "bg-slate-100 text-center cursor-pointer"
+        )}
+      >
+        <TooltipTrigger className={cn("h-full",isDisabled && "cursor-default")}>
+          {!isHovered && (
+            <Typography variant="p" className={cn("text-slate-600", isHoliday || (isDisabled && "text-slate-400"))}>
+              {data?.hours ? floatToTime(data?.hours || 0) : "-"}
+            </Typography>
+          )}
+          {isHovered && data?.hours && <PencilLine className="text-center" size={16} />}
+          {isHovered && !data?.hours && <CirclePlus className="text-center" size={16} />}
+        </TooltipTrigger>
+        {data?.description && <TooltipContent>{data?.description}</TooltipContent>}
+      </TableCell>
+    </Tooltip>
+  );
+};
+
+const LeaveRow = ({ leaves, dates }: { leaves: Array<LeaveProps>; dates: string[] }) => {
+  let total_hours = 0;
+  return (
+    <TableRow className="flex">
+      <TableCell className="w-full min-w-md text-left max-w-md">
+        <Typography variant="p" className="text-slate-800 w-full">
+          Time Off
+        </Typography>
+      </TableCell>
+      {dates.map((date: string) => {
+        const data = leaves.find((data: LeaveProps) => {
+          return date >= data.from_date && date <= data.to_date;
+        });
+        const hour = data?.half_day ? 4 : 8;
+        if (data) {
+          total_hours += hour;
+        }
+        return (
+          <TableCell key={date} className="max-w-20 min-w-20 w-full">
+            <Typography variant="p" className="text-slate-600">
+              {data ? floatToTime(hour) : "-"}
+            </Typography>
+          </TableCell>
+        );
+      })}
+      <TableCell className="max-w-24 w-full">
+        <Typography variant="p" className="text-slate-800 font-medium">
+          {floatToTime(total_hours)}
+        </Typography>
+      </TableCell>
+    </TableRow>
+  );
+};
--- timesheet/src/app/pages/team/index.tsx
+++ timesheet/src/app/pages/team/index.tsx
@@ -1,18 +1,68 @@
 import { Button } from "@/app/components/ui/button";
-import { ChevronLeft, ChevronRight, Filter } from "lucide-react";
+import { ChevronLeft, ChevronRight, Filter, CircleCheck } from "lucide-react";
 import { ComboxBox } from "@/app/components/comboBox";
 import { useFrappeGetCall } from "frappe-react-sdk";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@/store";
+import {
+  setData,
+  setFetchAgain,
+  setWeekDate,
+  setProject,
+  setStart,
+  updateData,
+  setHasMore,
+  resetData,
+} from "@/store/team";
+import { useToast } from "@/app/components/ui/use-toast";
+import { parseFrappeErrorMsg, prettyDate, floatToTime, getFormatedDate, cn } from "@/lib/utils";
+import { useEffect } from "react";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/app/components/ui/accordion";
+import { Table, TableHead, TableHeader, TableRow, TableBody, TableCell } from "@/app/components/ui/table";
+import { Typography } from "@/app/components/typography";
+import { Avatar, AvatarFallback, AvatarImage } from "@/app/components/ui/avatar";
+import { Employee } from "./employee";
+import { addDays } from "date-fns";
 
 type ProjectProps = {
-    project_name: string;
-    name: string;
-}
+  project_name: string;
+  name: string;
+};
+type ItemDataProps = {
+  date: string;
+  hour: number;
+};
+type ItemProps = {
+  employee_name: string;
+  name: string;
+  image: string;
+  data: Array<ItemDataProps>;
+};
+
+type DateProps = {
+  start_date: string;
+  end_date: string;
+  key: string;
+  dates: Array<string>;
+};
 
 const Team = () => {
+  const { toast } = useToast();
+  const teamState = useSelector((state: RootState) => state.team);
+  const dispatch = useDispatch();
+
   const { data: projects } = useFrappeGetCall("frappe.client.get_list", {
     doctype: "Project",
     fields: ["name", "project_name"],
   });
+
+  const { data, error } = useFrappeGetCall("timesheet_enhancer.api.team.get_compact_view_data", {
+    date: teamState.weekDate,
+    max_week: 1,
+    project: teamState.project,
+    start: teamState.start,
+  });
+
   const approvals = [
     { label: "Not Submitted", value: "Not Submitted" },
     { label: "Approval Pending", value: "Approval Pending" },
@@ -20,9 +70,53 @@ const Team = () => {
     { label: "Rejected", value: "Rejected" },
   ];
 
+  useEffect(() => {
+    if (data) {
+      if (Object.keys(teamState.data.data).length > 0) {
+        dispatch(updateData(data.message));
+      } else {
+        dispatch(setData(data.message));
+      }
+      dispatch(setHasMore(data.message.has_more));
+    }
+    if (error) {
+      const err = parseFrappeErrorMsg(error);
+      toast({
+        variant: "destructive",
+        description: err,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, error]);
+
+  const handleprevWeek = () => {
+    const date = getFormatedDate(addDays(teamState.weekDate, -6));
+    dispatch(setStart(0));
+    dispatch(resetData());
+    dispatch(setWeekDate(date));
+    dispatch(setFetchAgain(true));
+  };
+  const handlenextWeek = () => {
+    const date = getFormatedDate(addDays(teamState.weekDate, 6));
+    dispatch(setStart(0));
+    dispatch(resetData());
+    dispatch(setWeekDate(date));
+    dispatch(setFetchAgain(true));
+  };
+  const handleProjectChange = (value: string | string[]) => {
+    dispatch(setProject(value as string[]));
+    dispatch(setStart(0));
+    dispatch(resetData());
+    dispatch(setFetchAgain(true));
+  };
+  const handleLoadMore = () => {
+    if (!teamState.hasMore) return;
+    dispatch(setStart(teamState.start + 10));
+    dispatch(setFetchAgain(true));
+  };
   return (
     <>
-      <div className="flex gap-x-2 items-center justify-between">
+      <div className="flex gap-x-2 items-center justify-between mb-3">
         <div id="filters" className="flex gap-x-2">
           <ComboxBox
             label="Approval"
@@ -30,12 +124,14 @@ const Team = () => {
             isMulti
             leftIcon={<Filter className="h-4 w-4" />}
             className="text-primary border-dashed gap-x-2 font-normal"
+            disabled
           />
           <ComboxBox
             label="Team Groups"
             isMulti
+            onSelect={handleProjectChange}
             leftIcon={<Filter className="h-4 w-4" />}
-            data={projects?.message.map((item:ProjectProps) => ({
+            data={projects?.message.map((item: ProjectProps) => ({
               label: item.project_name,
               value: item.name,
             }))}
@@ -43,14 +139,86 @@ const Team = () => {
           />
         </div>
         <div id="date-filter" className="flex gap-x-2">
-          <Button className="p-1 h-fit" variant="outline">
+          <Button className="p-1 h-fit" variant="outline" onClick={handleprevWeek}>
             <ChevronLeft />
           </Button>
-          <Button className="p-1 h-fit" variant="outline">
+          <Button className="p-1 h-fit" variant="outline" onClick={handlenextWeek}>
             <ChevronRight />
           </Button>
         </div>
       </div>
+      <div className="overflow-y-scroll mb-3" style={{ height: "calc(100vh - 14rem)" }}>
+        <Table>
+          <TableHeader>
+            <TableRow className="flex items-center w-full">
+              <TableHead className="w-full max-w-md flex items-center">Members</TableHead>
+              {teamState.data?.dates.map((item: DateProps) => {
+                return item?.dates?.map((date) => {
+                  const { date: dateStr, day } = prettyDate(date);
+                  return (
+                    <TableHead className="flex flex-col max-w-20 w-full">
+                      <Typography variant="p" className="text-slate-600">
+                        {day}
+                      </Typography>
+                      <Typography variant="small" className="text-slate-500">
+                        {dateStr}
+                      </Typography>
+                    </TableHead>
+                  );
+                });
+              })}
+              <TableHead className="w-full max-w-24 flex items-center">Total</TableHead>
+              <TableHead className="w-full max-w-20 flex items-center">
+                <CircleCheck />
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+            {/* @ts-ignore */}
+            {Object.entries(teamState.data?.data).map(([key, item]: [string, ItemProps]) => {
+              let total = 0;
+              return (
+                <TableRow className="flex items-center w-full">
+                  <Accordion type="multiple" key={key} className="w-full">
+                    <AccordionItem value={key} className="border-b-0">
+                      <AccordionTrigger className="hover:no-underline py-2">
+                        <span className="w-full flex">
+                          <TableCell className="w-full max-w-md flex gap-x-2 items-center font-normal">
+                            <Avatar className="w-8 h-8">
+                              <AvatarImage src={decodeURIComponent(item.image)} />
+                              <AvatarFallback>{item.employee_name[0]}</AvatarFallback>
+                            </Avatar>
+                            {item.employee_name}
+                          </TableCell>
+                          {item.data.map((data: ItemDataProps) => {
+                            total += data.hour;
+                            return (
+                              <TableCell className={cn("max-w-20 w-full text-left")}>
+                                <Typography className={cn(data.hour > 8 && "text-warning")} variant="p">
+                                  {data.hour ? floatToTime(data.hour) : "-"}
+                                </Typography>
+                              </TableCell>
+                            );
+                          })}
+                          <TableCell className="w-full max-w-24 text-left">{floatToTime(total)}</TableCell>
+                        </span>
+                      </AccordionTrigger>
+                      <AccordionContent className="pb-0">
+                        <Employee employee={item.name} />
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+
+      <Button variant="outline" onClick={handleLoadMore} disabled={!teamState.hasMore}>
+        Load More
+      </Button>
     </>
   );
 };
