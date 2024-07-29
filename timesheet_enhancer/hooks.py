ee9b7851985c9ad409dffa4e8f6dde8b559955f1
14987 <nirajgautam196@gmail.com>
2024-07-29 16:57:30 +0530

    fix:Add doc events for task & timesheet

--- timesheet_enhancer/hooks.py
+++ timesheet_enhancer/hooks.py
@@ -161,7 +161,9 @@ doc_events = {
     "Timesheet": {
         "validate": "timesheet_enhancer.doc_events.timesheet.validate",
         "before_save": "timesheet_enhancer.doc_events.timesheet.before_save",
-    }
+        "before_insert": "timesheet_enhancer.doc_events.timesheet.before_insert",
+    },
+    "Task": {"before_save": "timesheet_enhancer.doc_events.task.before_save"},
 }
 
 # Scheduled Tasks
