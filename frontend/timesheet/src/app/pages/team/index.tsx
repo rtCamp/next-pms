c59034b56f5c82bd9d4b9a6ef6d5db5da77f1417
14987 <nirajgautam196@gmail.com>
2024-09-17 18:11:37 +0530

    fix: Update billing hour if actual hour has changed

--- frontend/timesheet/src/app/pages/team/index.tsx
+++ frontend/timesheet/src/app/pages/team/index.tsx
@@ -502,16 +502,16 @@ const Team = () => {
   );
 };
 
-const Status = ({ status }: { status: string }) => {
+export const Status = ({ status, className }: { status: string; className?: string }) => {
   if (status === "Approval Pending") {
-    return <Hourglass className="w-4 h-4 stroke-warning " />;
+    return <Hourglass className={cn("w-4 h-4 stroke-warning", className)} />;
   }
   if (status === "Approved" || status === "Partially Approved") {
-    return <CircleCheck className="w-4 h-4 stroke-success" />;
+    return <CircleCheck className={cn("w-4 h-4 stroke-success", className)} />;
   }
   if (status === "Rejected" || status === "Partially Rejected") {
-    return <CircleX className="w-4 h-4 stroke-destructive" />;
+    return <CircleX className={cn("w-4 h-4 stroke-destructive", className)} />;
   }
-  return <CircleCheck className="w-4 h-4 " />;
+  return <CircleCheck className={cn("w-4 h-4", className)} />;
 };
 export default Team;
