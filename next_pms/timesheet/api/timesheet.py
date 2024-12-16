360991ed4f8862a0ec3a581c62f3db1a26df8e4c
Niraj Gautam <nirajgautam196@gmail.com>
2024-12-16 16:41:04 +0530

    fix: Update permission check condition

--- next_pms/timesheet/api/timesheet.py
+++ next_pms/timesheet/api/timesheet.py
@@ -8,7 +8,11 @@ from frappe.utils import (
     nowdate,
 )
 
-from .employee import get_employee_daily_working_norm, get_employee_from_user, get_employee_working_hours
+from .employee import (
+    get_employee_daily_working_norm,
+    get_employee_from_user,
+    get_employee_working_hours,
+)
 from .utils import (
     get_holidays,
     get_leaves_for_employee,
@@ -27,7 +31,7 @@ def get_timesheet_data(employee: str, start_date=now, max_week: int = 4):
 
     if not employee:
         employee = get_employee_from_user()
-    if frappe.session.user != "Administrator" or (
+    if frappe.session.user != "Administrator" and (
         "Timesheet Manager" not in user_roles and "Timesheet User" not in user_roles
     ):
         frappe.has_permission("Employee", "read", employee, throw=True)
@@ -182,7 +186,12 @@ def submit_for_approval(approver: str, start_date: str, notes: str = None, emplo
         frappe.db.set_value("Timesheet", timesheet.name, "custom_approval_status", "Approval Pending")
 
     for timesheet in timesheets:
-        frappe.db.set_value("Timesheet", timesheet.name, "custom_weekly_approval_status", "Approval Pending")
+        frappe.db.set_value(
+            "Timesheet",
+            timesheet.name,
+            "custom_weekly_approval_status",
+            "Approval Pending",
+        )
 
     send_approval_reminder(employee, reporting_manager, start_date, end_date, notes)
     return f"Timesheet has been sent for Approval to {reporting_manager_name}."
