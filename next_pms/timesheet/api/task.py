26e9e96fe1bb625e96e46981a047f71e37b0c2b5
zeel-codder <zeelprajapati321@gmail.com>
2024-12-26 19:19:29 +0530

    refactor: update get_task order by condition

--- next_pms/timesheet/api/task.py
+++ next_pms/timesheet/api/task.py
@@ -161,8 +161,8 @@ def get_task(task: str, start_date: str | datetime.date, end_date: str | datetim
         )
         .where(timesheet_detail.task == task.name)
         .where((timesheet_detail.from_time >= start_date) & (timesheet_detail.to_time <= end_date))
-        .orderby(timesheet.employee_name, order=frappe.qb.asc)
         .groupby(timesheet.employee)
+        .orderby(timesheet.employee_name, order=frappe.qb.asc)
     ).run(as_dict=True)
 
     return {
