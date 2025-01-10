a93895709b0116ae7a0602a40f2141e8759771b5
Niraj Gautam <nirajgautam196@gmail.com>
2025-01-10 11:36:49 +0530

    Create timeshet table cell

--- frontend/packages/design-system/src/components/timesheet-table/components/cell.tsx
+++ frontend/packages/design-system/src/components/timesheet-table/components/cell.tsx
@@ -1,7 +1,14 @@
 /**
+ * External dependencies
+ */
+import { CircleDollarSign, CirclePlus, PencilLine } from "lucide-react";
+/**
  * Internal dependencies
  */
-import { HoverCard } from "@design-system/components/hover-card";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@design-system/components/hover-card";
+import { TableCell } from "@design-system/components/table";
+import Typography from "@design-system/components/typography";
+import { getBgCsssForToday, cn, floatToTime, preProcessLink } from "@design-system/utils";
 import { TaskDataItemProps } from "../type";
 
 type CellOnClickProps = {
@@ -46,6 +53,55 @@ const Cell = ({ date, data, isHoliday, onCellClick, disabled, cellClassName }: c
     };
     onCellClick && onCellClick(value);
   };
-  return <HoverCard openDelay={1000} closeDelay={0}></HoverCard>;
+  return (
+    <HoverCard openDelay={1000} closeDelay={0}>
+      <TableCell
+        key={date}
+        onClick={handleClick}
+        className={cn(
+          "text-center group px-2",
+          isDisabled && "cursor-default",
+          "hover:h-full hover:bg-slate-100 hover:cursor-pointer",
+          getBgCsssForToday(date),
+          cellClassName
+        )}
+      >
+        <HoverCardTrigger className={cn(isDisabled && "cursor-default")}>
+          <span className="flex flex-col items-center justify-center ">
+            <Typography
+              variant="p"
+              className={cn(
+                "text-slate-600",
+                isHoliday || (isDisabled && "text-slate-400"),
+                !hours && !isDisabled && "group-hover:hidden"
+              )}
+            >
+              {hours > 0 ? floatToTime(hours || 0) : "-"}
+            </Typography>
+
+            {(isTimeBothBillableAndNonBillable || isTimeBillable) && (
+              <CircleDollarSign
+                className={cn(
+                  "stroke-slate-500 w-4 h-4 ",
+                  !isDisabled && "group-hover:hidden",
+                  isTimeBillable && "stroke-success"
+                )}
+              />
+            )}
+            <PencilLine
+              className={cn("text-center hidden", hours > 0 && !isDisabled && "group-hover:block")}
+              size={16}
+            />
+            <CirclePlus className={cn("text-center hidden", !hours && !isDisabled && "group-hover:block ")} size={16} />
+          </span>
+        </HoverCardTrigger>
+        {description && (
+          <HoverCardContent className="text-left whitespace-pre text-wrap w-full max-w-96 max-h-52 overflow-auto">
+            <p dangerouslySetInnerHTML={{ __html: preProcessLink(description) }}></p>
+          </HoverCardContent>
+        )}
+      </TableCell>
+    </HoverCard>
+  );
 };
 export default Cell;
