95f0b26996fa77cfbcd28196e0eda051caabf09d
Niraj Gautam <nirajgautam196@gmail.com>
2025-01-13 21:35:50 +0530

    Init workspace app and move src into it

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,39 +1,39 @@
-import path from "path";
+import { resolve } from 'path'
 import react from "@vitejs/plugin-react";
+import tailwindcss from 'tailwindcss';
 import { defineConfig, loadEnv } from "vite";
-
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  let proxyConfig = {}
-  if (env.VITE_SITE_NAME && env.VITE_SITE_PORT) {
-    proxyConfig = {
-      "^/(app|api|assets|files|private)": {
-        target: `http://localhost:${env.VITE_SITE_PORT}`,
-        ws: true,
-        changeOrigin: true,
-        secure: false,
-        router: function (req) {
-          return `http://${env.VITE_SITE_NAME}:${env.VITE_SITE_PORT}`;
-        },
-      }
+    const env = loadEnv(mode, process.cwd(), '')
+    let proxyConfig = {}
+    if (env.VITE_SITE_NAME && env.VITE_SITE_PORT) {
+        proxyConfig = {
+            "^/(app|api|assets|files|private)": {
+                target: `http://localhost:${env.VITE_SITE_PORT}`,
+                ws: true,
+                changeOrigin: true,
+                secure: false,
+                router: function (req) {
+                    return `http://${env.VITE_SITE_NAME}:${env.VITE_SITE_PORT}`;
+                },
+            }
+        }
     }
-  }
-  return {
-    plugins: [react()],
-    server: {
-      port: 5173,
-      proxy: proxyConfig,
-    },
-    build: {
-      outDir: "../next_pms/public/frontend",
-      emptyOutDir: true,
-      target: "es2015",
-    },
-    resolve: {
-      alias: {
-        // eslint-disable-next-line no-undef
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-  }
+    return {
+        root: resolve(__dirname, './packages/app'),
+        plugins: [react(), tailwindcss()],
+        server: {
+            port: 5173,
+            proxy: proxyConfig,
+        },
+        resolve: {
+            alias: {
+                "@": resolve(__dirname, "./packages/app/src"),
+            },
+        },
+        build: {
+            outDir: "../../../next_pms/public/frontend",
+            emptyOutDir: true,
+            target: "es2015",
+        },
+    }
 })
