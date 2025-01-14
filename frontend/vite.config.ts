6e757bd46c48334b9cc1c353d2c0e7b12fc7414b
Niraj Gautam <nirajgautam196@gmail.com>
2025-01-14 15:51:14 +0530

    Fix env file path in proxy options

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,18 +3,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from 'tailwindcss';
 import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+    const env = loadEnv(mode, "../../",'')
     let proxyConfig = {}
     if (env.VITE_SITE_NAME && env.VITE_SITE_PORT) {
         proxyConfig = {
             "^/(app|api|assets|files|private)": {
-                target: `http://localhost:${env.VITE_SITE_PORT}`,
+                target: `http://${env.VITE_SITE_NAME}:${env.VITE_SITE_PORT}`,
                 ws: true,
                 changeOrigin: true,
                 secure: false,
-                router: function (req) {
-                    return `http://${env.VITE_SITE_NAME}:${env.VITE_SITE_PORT}`;
-                },
             }
         }
     }
