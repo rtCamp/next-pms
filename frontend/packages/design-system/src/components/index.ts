efa980cf266da0682347e1456d3c7cb4cf8d0a1a
Niraj Gautam <nirajgautam196@gmail.com>
2025-01-15 17:54:03 +0530

    Add input component

--- frontend/packages/design-system/src/components/index.ts
+++ frontend/packages/design-system/src/components/index.ts
@@ -4,3 +4,5 @@
 export { default as Button, type ButtonProps } from "./button";
 export { default as Badge, type BadgeProps } from "./badge";
 export { default as Checkbox } from "./checkbox";
+export { default as DeBouncedInput, type DeBounceInputProps } from "./debounced-input";
+export { default as Input, type InputProps } from "./input";
