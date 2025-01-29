1b50e4fc5e0b0de5788a2cd143569a2cb34080b7
zeel-codder <zeelprajapati321@gmail.com>
2025-01-29 15:47:48 +0530

    Resolve the imports as per file name in resource management code

--- frontend/packages/app/src/app/pages/resource_management/project/index.tsx
+++ frontend/packages/app/src/app/pages/resource_management/project/index.tsx
@@ -15,7 +15,7 @@ import { AllocationDataProps, PermissionProps } from "@/store/resource_managemen
 import { setData, setReFetchData, updateData } from "@/store/resource_management/project";
 
 import AddResourceAllocations from "../components/AddAllocation";
-import { ResourceProjectHeaderSection } from "./components/Header";
+import { ResourceProjectHeaderSection } from "./components/header";
 import { getIsBillableValue } from "../utils/helper";
 import { ResourceProjectTable } from "./components/table";
 
