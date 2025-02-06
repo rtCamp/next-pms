e095faedefae84fd300d7c68ba48604e964e79ae
Alok Singh <alokmilenium@gmail.com>
2025-02-06 18:20:48 +0530

    refactor: Simplify ESLint config by removing React plugin
    Signed-off-by: Alok Singh <alokmilenium@gmail.com>

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,7 +3,6 @@
  */
 import js from '@eslint/js'
 import globals from 'globals'
-import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -13,30 +12,17 @@ import parser from '@typescript-eslint/parser'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...reactPlugin.configs.recommended,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       parser: parser,
-      ecmaVersion: 2024,
-      globals: {
-        ...globals.browser,
-        React: 'readonly',
-      },
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
     plugins: {
-      'react': reactPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      'import': importPlugin,
+      "import": importPlugin
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -44,41 +30,34 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-react': 'off',
-      'import/order': [
-        'error',
+      "import/order": [
+        "error",
         {
-          'groups': ['builtin', 'external', 'internal'],
-          'pathGroups': [
+          "groups": ["builtin", "external", "internal"],
+          "pathGroups": [
             {
-              pattern: 'react',
-              group: 'external',
-              position: 'before',
+              pattern: "react",
+              group: "external",
+              position: "before",
             },
             {
-              pattern: 'react-*',
-              group: 'external',
-              position: 'before',
+              pattern: "react-*",
+              group: "external",
+              position: "before",
             },
             {
-              pattern: '@/**',
-              group: 'internal',
-              position: 'after',
+              pattern: "@/**",
+              group: "internal",
+              position: "after",
             },
           ],
-          'pathGroupsExcludedImportTypes': ['builtin'],
-          'alphabetize': {
-            order: 'asc',
+          "pathGroupsExcludedImportTypes": ["builtin"],
+          "alphabetize": {
+            order: "asc",
             caseInsensitive: true,
           },
         },
       ],
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
     },
   },
 )
