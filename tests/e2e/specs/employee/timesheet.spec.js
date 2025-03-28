5b50bcf61b2bce069e52d7cfce6ca0dc21496ac6
maharrshi <manoj.maharrshi.r.m@rtcamp.com>
2025-03-28 16:37:17 +0530

    Included .only to run the scripts that has a proper prerequisite

--- tests/e2e/specs/employee/timesheet.spec.js
+++ tests/e2e/specs/employee/timesheet.spec.js
@@ -29,7 +29,7 @@ test.beforeEach(async ({ page }) => {
 
 // ------------------------------------------------------------------------------------------
 
-test("TC2: Time should be added using the ‘Add’ button at the top.", async ({ page }) => {
+test.only("TC2: Time should be added using the ‘Add’ button at the top.", async ({ page }) => {
   // Add time entry using "Time" button
   await timesheetPage.addTimeViaTimeButton(TC2data.taskInfo);
 
@@ -59,7 +59,7 @@ test("TC3: Time should be added using the direct timesheet add buttons.", async 
   expect(cellText).toContain(TC3data.taskInfo.duration);
 });
 
-test("TC4: Added time and description should be editable.", async ({ page }) => {
+test.only("TC4: Added time and description should be editable.", async ({ page }) => {
   // Update time entry
 
   await timesheetPage.updateTimeRow(TC4data.cell, {
@@ -79,7 +79,7 @@ test("TC4: Added time and description should be editable.", async ({ page }) => 
   expect(cellTooltipText).toContain(TC4data.taskInfo.desc);
 });
 
-test("TC5: Add a new row in the already added time.", async ({ page }) => {
+test.only("TC5: Add a new row in the already added time.", async ({ page }) => {
   // Store cell text before new row addition
   const beforeCellText = await timesheetPage.getCellText(TC5data.cell);
 
