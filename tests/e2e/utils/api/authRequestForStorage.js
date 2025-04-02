0eb81eadf1cb439d88745f1218a7c3c38242e8de
Manoj Maharrshi <110522049+maharrshi@users.noreply.github.com>
2025-04-02 13:27:07 +0530

    Create authRequestForStorage.js
    function to login via API for front end

---
+++ tests/e2e/utils/api/authRequestForStorage.js
@@ -0,0 +1,16 @@
+export const loginIntoNextPMS = async (requestContext, email, password) => {
+    const response = await requestContext.post(`/login`, {
+      data: {
+        cmd: "login",
+        usr: email,
+        pwd: password,
+      },
+    });
+  
+    // Ensure login was successful
+    if (!response.ok()) {
+      throw new Error(`Login failed: ${response.status()} ${response.statusText()}`);
+    }
+  
+    return response; // No need to return context
+  };
