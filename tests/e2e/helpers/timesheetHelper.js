6d9a19317f0dea0d9359222daba764bde1897cf8
maharrshi <manoj.maharrshi.r.m@rtcamp.com>
2025-04-02 18:22:54 +0530

    lint issues

--- tests/e2e/helpers/timesheetHelper.js
+++ tests/e2e/helpers/timesheetHelper.js
@@ -155,7 +155,7 @@ export const filterTimesheetEntry = async ({ subject, description, project_name,
         (entry) =>
           entry.description === description &&
           entry.project_name === project_name &&
-          entry.from_time.includes(from_time),
+          entry.from_time.includes(from_time)
       );
 
       // Return the matching entry
