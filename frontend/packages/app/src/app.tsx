bffc265fb9bdab0f4ededba6fb9a490b7c4fad82
Niraj Gautam <nirajgautam196@gmail.com>
2025-05-18 19:00:35 +0530

    feat: Show version update toast

--- frontend/packages/app/src/app.tsx
+++ frontend/packages/app/src/app.tsx
@@ -5,13 +5,14 @@ import { Suspense } from "react";
 import { Provider } from "react-redux";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import { ErrorFallback, TooltipProvider } from "@next-pms/design-system/components";
-import { FrappeProvider } from "frappe-react-sdk";
+
 /**
  * Internal dependencies.
  */
+
 import { BASE_ROUTE } from "@/lib/constant";
 import { UserProvider } from "@/lib/UserProvider";
-import { getSiteName, enableSocket } from "@/lib/utils";
+import FrappeProvider from "./providers/frappe";
 import ThemeProvider from "./providers/theme";
 import { Router } from "./route";
 import { store } from "./store";
@@ -21,28 +22,21 @@ const App = () => {
   });
 
   return (
-    <>
-      <FrappeProvider
-        url={import.meta.env.VITE_BASE_URL ?? ""}
-        socketPort={import.meta.env.VITE_SOCKET_PORT}
-        enableSocket={enableSocket()}
-        siteName={getSiteName()}
-      >
-        <ThemeProvider>
-          <UserProvider>
-            <Provider store={store}>
-              <TooltipProvider>
-                <Suspense fallback={<></>}>
-                  <ErrorFallback>
-                    <RouterProvider router={router} />
-                  </ErrorFallback>
-                </Suspense>
-              </TooltipProvider>
-            </Provider>
-          </UserProvider>
-        </ThemeProvider>
-      </FrappeProvider>
-    </>
+    <FrappeProvider>
+      <ThemeProvider>
+        <UserProvider>
+          <Provider store={store}>
+            <TooltipProvider>
+              <Suspense fallback={<></>}>
+                <ErrorFallback>
+                  <RouterProvider router={router} />
+                </ErrorFallback>
+              </Suspense>
+            </TooltipProvider>
+          </Provider>
+        </UserProvider>
+      </ThemeProvider>
+    </FrappeProvider>
   );
 };
 
