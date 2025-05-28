b2a2bd1c018e06cb8a3a9f43fd256676cd6d6237
Niraj Gautam <nirajgautam196@gmail.com>
2025-05-28 11:23:13 +0530

    refactor: Move add time component to respective folder

--- frontend/packages/app/src/app/components/types.ts
+++ frontend/packages/app/src/app/components/types.ts
@@ -3,26 +3,6 @@
  */
 import { ReactNode } from "react";
 
-/**
- * Internal Dependencies
- */
-import { WorkingFrequency } from "@/types";
-
-export interface AddTimeProps {
-  initialDate: string;
-  employee: string;
-  open: boolean;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  onOpenChange: (data: any) => void;
-  workingHours: number;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  onSuccess?: (data: any) => void;
-  workingFrequency: WorkingFrequency;
-  task?: string;
-  project?: string;
-  employeeName?: string;
-}
-
 export interface EmployeeComboProps {
   disabled?: boolean;
   value: string;
