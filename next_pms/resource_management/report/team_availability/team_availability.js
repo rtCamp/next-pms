5fcdd57508b3914446340ef700a8c2b57d9d51e8
Niraj Gautam <nirajgautam196@gmail.com>
2025-06-13 18:14:20 +0530

    feat: Add team availability report

---
+++ next_pms/resource_management/report/team_availability/team_availability.js
@@ -0,0 +1,100 @@
+// Copyright (c) 2025, rtCamp and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Team Availability"] = {
+  filters: [],
+  tree: true,
+  name_field: "name",
+  parent_field: "reports_to",
+  initial_depth: 2,
+  formatter: function (value, row, column, data, default_formatter) {
+    value = default_formatter(value, row, column, data);
+
+    if (data.has_value && ["name", "designation", "available_capacity"].includes(column.id)) {
+      var $value = $(value).css("font-weight", "bold");
+      value = $value.wrap("<p></p>").parent().html();
+    }
+    if (column.id === "employee_name" && data.has_value) {
+      value = `<span style="font-weight: bold;">${value}</span>`;
+    }
+    return value;
+  },
+};
+
+const setup_filters = () => {
+  frappe.query_reports["Team Availability"].filters.push({
+    fieldname: "from",
+    label: __("From Date"),
+    fieldtype: "Date",
+    default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+    reqd: 1,
+  });
+  frappe.query_reports["Team Availability"].filters.push({
+    fieldname: "to",
+    label: __("To Date"),
+    fieldtype: "Date",
+    default: frappe.datetime.get_today(),
+    reqd: 1,
+  });
+
+  const employeeDoctype = "Employee";
+  const buDoctype = "Business Unit";
+
+  frappe.model.with_doctype(employeeDoctype, () => {
+    let meta = frappe.get_meta(employeeDoctype);
+    const fields = meta.fields;
+
+    const statusField = fields.find((field) => field.fieldtype === "Select" && field.fieldname === "status");
+    if (statusField) {
+      frappe.query_reports["Team Availability"].filters.push({
+        fieldname: "status",
+        label: __("Status"),
+        fieldtype: "Select",
+        options: "\n" + statusField.options,
+        default: "Active",
+        reqd: 0,
+      });
+    }
+
+    const buField = fields.find((field) => field.fieldtype === "Link" && field.options === buDoctype);
+    if (buField) {
+      frappe.query_reports["Team Availability"].filters.push({
+        fieldname: "business_unit",
+        label: __("Business Unit"),
+        fieldtype: "MultiSelectList",
+        options: buDoctype,
+        reqd: 0,
+        get_data: function (txt) {
+          return frappe.db.get_link_options(buDoctype, txt);
+        },
+      });
+    }
+    frappe.query_reports["Team Availability"].filters.push({
+      fieldname: "designation",
+      label: __("Designation"),
+      fieldtype: "MultiSelectList",
+      options: "Designation",
+      reqd: 0,
+      get_data: function (txt) {
+        return frappe.db.get_link_options("Designation", txt);
+      },
+    });
+    frappe.query_reports["Team Availability"].filters.push({
+      fieldname: "currency",
+      label: __("Currency"),
+      fieldtype: "Select",
+      options: ["INR", "USD"],
+      reqd: 0,
+      default: "USD",
+    });
+    frappe.query_reports["Team Availability"].filters.push({
+      fieldname: "is_group",
+      label: __("Aggregate by Manager"),
+      fieldtype: "Check",
+      default: 0,
+    });
+  });
+};
+
+// This function sets up the filters for the Team Availability report based on the Employee and Business Unit doctypes dynamically.
+setup_filters();
---
+++ next_pms/resource_management/report/team_availability/team_availability.py
@@ -0,0 +1,177 @@
+# Copyright (c) 2025, rtCamp and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+from frappe import _, get_meta
+from frappe.utils import getdate
+
+from next_pms.resource_management.api.utils.query import (
+    get_allocation_list_for_employee_for_given_range,
+)
+from next_pms.resource_management.report.utils import (
+    calculate_employee_available_hours,
+    calculate_employee_hours,
+)
+from next_pms.timesheet.api.employee import get_employee_daily_working_norm
+from next_pms.utils.employee import generate_flat_tree, get_employee_leaves_and_holidays
+
+from .utils import (
+    BU_FIELD_NAME,
+    convert_currency,
+    get_employee_fields,
+    get_employee_filters,
+    sort_by_reports_to,
+)
+
+
+def execute(filters=None):
+    employee_meta = get_meta("Employee")
+    has_bu_field = employee_meta.has_field(BU_FIELD_NAME)
+
+    columns = get_columns(filters)
+    data = get_data(filters, has_bu_field)
+    return columns, data
+
+
+def get_data(filters=None, has_bu_field=False):
+    """
+    Fetches the team availability data based on the provided filters.
+    """
+    start_date = filters.get("from")
+    end_date = filters.get("to")
+    is_group = filters.get("is_group", False)
+    currency = filters.get("currency") or "USD"
+    fields = get_employee_fields(has_bu_field)
+
+    data = generate_flat_tree(
+        doctype="Employee",
+        fields=fields,
+        filters=get_employee_filters(filters, has_bu_field),
+        nsm_field="reports_to",
+    )
+
+    employees = data.get("level", [])
+    parent_child_map = data.get("with_children", {})
+
+    for emp in employees:
+        emp.indent = emp.level or 0
+        emp.has_value = len(parent_child_map.get(emp.name, {}).get("childrens", [])) > 0
+
+    employees = sort_by_reports_to(employees)
+
+    employee_names = [emp.name for emp in employees]
+    resource_allocation_map = get_allocations(start_date, end_date, employee_names)
+    for emp in employees:
+        daily_hours = get_employee_daily_working_norm(emp.name)
+        employee_allocations = resource_allocation_map.get(emp.name, [])
+        non_billable_allocations = [resource for resource in employee_allocations if not resource.is_billable]
+        employee_leave_and_holiday = get_employee_leaves_and_holidays(emp.name, getdate(start_date), getdate(end_date))
+        holidays = employee_leave_and_holiday.get("holidays")
+        leaves = employee_leave_and_holiday.get("leaves")
+
+        employee_non_billable_hours = calculate_employee_hours(
+            daily_hours,
+            start_date,
+            end_date,
+            non_billable_allocations,
+            holidays,
+            leaves,
+        )
+        employee_free_hours = calculate_employee_available_hours(
+            daily_hours, start_date, end_date, employee_allocations, holidays, leaves
+        )
+        emp.available_capacity = employee_free_hours + employee_non_billable_hours
+
+        # Calculate monthly salary
+        emp.monthly_salary = emp.ctc / 12
+        if emp.currency != currency:
+            emp.monthly_salary = convert_currency(emp.monthly_salary, emp.currency, currency)
+            emp.currency = currency
+
+    if is_group:
+        employee_map = {emp.name: emp for emp in employees}
+        root_nodes = [emp.name for emp in employees if emp.has_value]
+        for emp in root_nodes:
+            childrens = parent_child_map.get(emp, {}).get("childrens", [])
+            child_names = [child.name for child in childrens if child.name in employee_names]
+            hours = sum(employee_map[c].available_capacity for c in child_names)
+            root_emp = employee_map[emp]
+            root_emp.available_capacity += hours
+
+            # Calculate monthly salary for root nodes (group)
+            root_emp.monthly_salary = sum(employee_map[c].monthly_salary for c in child_names)
+    return employees
+
+
+def get_allocations(start_date, end_date, employee_names):
+    resource_allocations = get_allocation_list_for_employee_for_given_range(
+        columns=get_resource_allocation_fields(),
+        value_key="employee",
+        values=employee_names,
+        start_date=start_date,
+        end_date=end_date,
+    )
+    resource_allocation_map = {}
+    for resource_allocation in resource_allocations:
+        if resource_allocation.employee not in resource_allocation_map:
+            resource_allocation_map[resource_allocation.employee] = []
+        resource_allocation_map[resource_allocation.employee].append(resource_allocation)
+    return resource_allocation_map
+
+
+def get_columns(filters=None):
+    currency = filters.get("currency") or "USD"
+    return [
+        {
+            "fieldname": "name",
+            "label": _("ID"),
+            "fieldtype": "Link",
+            "options": "Employee",
+            "width": 200,
+        },
+        {
+            "fieldname": "employee_name",
+            "label": _("Employee Name"),
+            "fieldtype": "Data",
+            "width": 200,
+        },
+        {
+            "fieldname": "designation",
+            "label": _("Designation"),
+            "fieldtype": "Link",
+            "options": "Designation",
+            "width": 200,
+        },
+        {
+            "label": _("Available Capacity (hrs)"),
+            "fieldname": "available_capacity",
+            "fieldtype": "Float",
+            "width": 150,
+        },
+        {
+            "label": _("Monthly Salary ({0})").format(currency),
+            "fieldname": "monthly_salary",
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 130,
+        },
+    ]
+
+
+def get_resource_allocation_fields():
+    return [
+        "name",
+        "employee",
+        "employee_name",
+        "allocation_start_date",
+        "allocation_end_date",
+        "hours_allocated_per_day",
+        "project",
+        "project_name",
+        "customer",
+        "is_billable",
+        "note",
+        "total_allocated_hours",
+    ]
