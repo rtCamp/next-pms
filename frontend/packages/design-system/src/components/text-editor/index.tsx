f9d5dedeec2513b776df9a8437909674b0360783
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2025-06-25 12:12:05 +0530

    Fix word break in text-editor component

--- frontend/packages/design-system/src/components/text-editor/index.tsx
+++ frontend/packages/design-system/src/components/text-editor/index.tsx
@@ -85,8 +85,9 @@ const TextEditor = ({
         {...Props}
         style={{ resize: "vertical", overflow: "auto" }}
         className={mergeClassNames(
-          "border rounded-md border-input [&>div:first-child]:border-t-0 [&>div:first-child]:border-r-0 [&>div:first-child]:border-l-0 [&>div:first-child]:border-input [&>div:first-child]:border-bottom [&>div:last-child]:border-none text-foreground bg-background break-all whitespace-normal",
+          "border rounded-md border-input [&>div:first-child]:border-t-0 [&>div:first-child]:border-r-0 [&>div:first-child]:border-l-0 [&>div:first-child]:border-input [&>div:first-child]:border-bottom [&>div:last-child]:border-none text-foreground bg-background whitespace-normal",
           hideToolbar && "border-none !resize-none [&_.ql-editor]:min-h-0 [&_.ql-editor]:p-2",
+          !hideToolbar && "break-all",
           className
         )}
         theme="snow"
