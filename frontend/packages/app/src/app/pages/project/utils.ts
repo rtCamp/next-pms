d213f3dc161229def561b2fa68c2ad5e9c61c583
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2025-07-21 16:26:53 +0530

    Add multiple tag filter support

--- frontend/packages/app/src/app/pages/project/utils.ts
+++ frontend/packages/app/src/app/pages/project/utils.ts
@@ -4,40 +4,49 @@
 import { ProjectState } from "@/store/project";
 
 export const createFilter = (projectState: ProjectState) => {
-    return {
-        search: projectState.search,
-        project_type: projectState.selectedProjectType,
-        status: projectState.selectedStatus,
-        business_unit: projectState.selectedBusinessUnit,
-        currency: projectState.currency,
-        billing_type: projectState.selectedBillingType
-    }
-}
-
-export const getFilter = (projectState: ProjectState) => {
-    const filters = [];
-
-    if (projectState.search) {
-        filters.push(["project_name", "like", `%${projectState.search}%`]);
-    }
-    if (projectState.selectedProjectType.length > 0) {
-        filters.push(["project_type", "in", projectState.selectedProjectType]);
-    }
-    if (projectState.selectedStatus.length > 0) {
-        filters.push(["status", "in", projectState.selectedStatus]);
-    }
-    if (projectState.selectedBusinessUnit.length > 0) {
-        filters.push(["custom_business_unit", "in", projectState.selectedBusinessUnit]);
-    }
-    if (projectState.selectedBillingType.length > 0) {
-        filters.push(["custom_billing_type", "in", projectState.selectedBillingType]);
-    }
-    if(projectState.tag.length > 0){
-        filters.push(["_user_tags", "like", `%${projectState.tag[0]}%`]);
-    }
-    return filters;
+  return {
+    search: projectState.search,
+    project_type: projectState.selectedProjectType,
+    status: projectState.selectedStatus,
+    business_unit: projectState.selectedBusinessUnit,
+    currency: projectState.currency,
+    billing_type: projectState.selectedBillingType,
+  };
 };
 
+export const getFilter = (projectState: ProjectState) => {
+  const filters = [];
+
+  if (projectState.search) {
+    filters.push(["project_name", "like", `%${projectState.search}%`]);
+  }
+  if (projectState.selectedProjectType.length > 0) {
+    filters.push(["project_type", "in", projectState.selectedProjectType]);
+  }
+  if (projectState.selectedStatus.length > 0) {
+    filters.push(["status", "in", projectState.selectedStatus]);
+  }
+  if (projectState.selectedBusinessUnit.length > 0) {
+    filters.push([
+      "custom_business_unit",
+      "in",
+      projectState.selectedBusinessUnit,
+    ]);
+  }
+  if (projectState.selectedBillingType.length > 0) {
+    filters.push([
+      "custom_billing_type",
+      "in",
+      projectState.selectedBillingType,
+    ]);
+  }
+  if (projectState.tag.length > 0) {
+    projectState.tag.forEach((tag) => {
+      filters.push(["Project", "_user_tags", "like", `%${tag}%`]);
+    });
+  }
+  return filters;
+};
 
 export const getValidUserTagsValues = (input: string) => {
   if (!input) return []; // Handle null, undefined, or empty input
