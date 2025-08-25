52c34ba2828d2aa201ccf945de2e75404bbd1828
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2025-08-25 20:46:21 +0530

    Add Project Status Update and Project Comments Doctype

---
+++ next_pms/next_pms/doctype/project_status_updates/test_project_status_updates.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, rtCamp and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProjectStatusUpdates(FrappeTestCase):
+    pass
