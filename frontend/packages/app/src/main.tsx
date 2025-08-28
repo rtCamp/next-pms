609a54717a34e4a4eb816e634889d71e920d7b2e
Niraj Gautam <nirajgautam196@gmail.com>
2025-08-28 12:54:49 +0530

    refactor: Add dev context for react app in development mode

--- frontend/packages/app/src/main.tsx
+++ frontend/packages/app/src/main.tsx
@@ -12,6 +12,18 @@ import "@next-pms/design-system/index.css";
 import "@next-pms/resource-management/index.css";
 import "./global.css";
 
+if (import.meta.env.DEV) {
+  fetch("/api/method/next_pms.www.next-pms.index.get_context_for_dev", {
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((values) => {
+      const v = JSON.parse(values.message);
+      if (!window.frappe) window.frappe = {};
+      window.frappe.boot = v;
+    });
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
