118db12220da8f443d0ee6d908966c30380a6dc5
wreckage0907 <girish.raghav2004@gmail.com>
2025-12-30 16:14:51 +0530

    chore: ran formatter

--- next_pms/public/js/customer.js
+++ next_pms/public/js/customer.js
@@ -1,14 +1,13 @@
 frappe.ui.form.on("Customer", {
-    customer_name: async function(frm) {
-        if(frm.doc.customer_name){
-            const abbr = frm.doc.customer_name
-                .split(" ")
-                .filter(Boolean)
-                .map(w => w[0])
-                .join("")
-                .toUpperCase();
-            frm.set_value("custom_abbr", abbr);
-        
-    }}
-
-})
+  customer_name: async function (frm) {
+    if (frm.doc.customer_name) {
+      const abbr = frm.doc.customer_name
+        .split(" ")
+        .filter(Boolean)
+        .map((w) => w[0])
+        .join("")
+        .toUpperCase();
+      frm.set_value("custom_abbr", abbr);
+    }
+  },
+});
