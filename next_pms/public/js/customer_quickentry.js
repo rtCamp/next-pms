574e0309950fd26a598e1033af79a0a5ce4cc998
wreckage0907 <girish.raghav2004@gmail.com>
2025-12-30 16:08:28 +0530

    feat: added autofill for abbreviation in quickentry and form view

---
+++ next_pms/public/js/customer_quickentry.js
@@ -0,0 +1,26 @@
+class CustomerQuickEntryForm extends frappe.ui.form.CustomerQuickEntryForm {
+
+    render_dialog() {
+        super.render_dialog();
+        console.log(this.dialog);
+
+        // Hook AFTER dialog exists
+        this.dialog.fields_dict.customer_name.df.onchange = () => {
+                this.set_abbr();
+        };
+    }
+
+    set_abbr() {
+        const name = this.dialog.doc.customer_name || "";
+
+        const abbr = name
+            .split(" ")
+            .filter(Boolean)
+            .map(w => w[0])
+            .join("")
+            .toUpperCase();
+
+        this.dialog.set_value("custom_abbr", abbr);
+    }
+}
+frappe.ui.form.CustomerQuickEntryForm = CustomerQuickEntryForm;
