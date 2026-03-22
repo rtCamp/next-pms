9c529e1c26cb4c131fa91e19cf8bbbc538c94b90
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2026-03-22 14:52:00 +0530

    refactor: removed unused view store
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- frontend/packages/app/src/route.tsx
+++ frontend/packages/app/src/route.tsx
@@ -1,17 +1,13 @@
 /**
  * External dependencies.
  */
-import { lazy, useContext, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { lazy } from "react";
 import { Route, Outlet } from "react-router-dom";
 /**
  * Internal dependencies.
  */
-import { FrappeConfig, FrappeContext } from "frappe-react-sdk";
 import { ROUTES } from "@/lib/constant";
-import { setViews } from "@/store/view";
 import { useUser } from "./providers/user";
-import { RootState } from "./store";
 /**
  * Lazy load components.
  */
@@ -55,25 +51,10 @@ export function Router() {
 }
 
 const AuthenticatedRoute = () => {
-  const { call } = useContext(FrappeContext) as FrappeConfig;
   const { isLoading: isUserLoading, currentUser } = useUser(({ state }) => ({
     isLoading: state.isLoading,
     currentUser: state.currentUser,
   }));
-  const views = useSelector((state: RootState) => state.view);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (views.views.length < 1) {
-      call
-        .get(
-          "next_pms.timesheet.doctype.pms_view_setting.pms_view_setting.get_views",
-        )
-        .then((res) => {
-          dispatch(setViews(res.message));
-        });
-    }
-  }, [call, dispatch, views.views.length]);
 
   if (isUserLoading) {
     return <></>;
--- frontend/packages/app/src/store/index.ts
+++ frontend/packages/app/src/store/index.ts
@@ -8,12 +8,10 @@ import { configureStore } from "@reduxjs/toolkit";
  */
 
 import projectReducer from "./project";
-import viewReducer from "./view";
 
 export const store = configureStore({
   reducer: {
     project: projectReducer,
-    view: viewReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
