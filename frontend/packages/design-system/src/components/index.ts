37d36cfa7181fbfbb3eaf89bfa4ede4787e50cc7
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2026-03-22 16:45:00 +0530

    refactor: remove command
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- frontend/packages/design-system/src/components/index.ts
+++ frontend/packages/design-system/src/components/index.ts
@@ -51,17 +51,6 @@ export {
   AccordionContent,
 } from "./accordion";
 export {
-  Command,
-  CommandDialog,
-  CommandInput,
-  CommandList,
-  CommandEmpty,
-  CommandGroup,
-  CommandItem,
-  CommandShortcut,
-  CommandSeparator,
-} from "./command";
-export {
   default as DurationInput,
   type DurationInputProps,
 } from "./durationInput";
