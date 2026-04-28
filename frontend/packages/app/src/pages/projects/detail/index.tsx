d902019faa2539917ffac90abb7e9065afb5e17b
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2026-04-28 12:32:16 +0530

    fix: tab overflowing
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- frontend/packages/app/src/pages/projects/detail/index.tsx
+++ frontend/packages/app/src/pages/projects/detail/index.tsx
@@ -8,7 +8,6 @@ import { Tabs } from "@rtcamp/frappe-ui-react";
 /**
  * Internal dependencies.
  */
-import { UnderConstruction } from "@/components/under-construction";
 import { FAKE_PROJECTS } from "@/pages/projects/list/fake-data";
 
 import { ProjectDetailHeader } from "./header";
@@ -23,13 +22,16 @@ function ProjectDetail() {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-    <div>
+    <div className="h-full flex flex-col">
       <ProjectDetailHeader projectName={projectName} />
-      <div className="flex">
-        <div className="flex min-w-0 flex-1 flex-col">
-          <Tabs tabs={TABS} tabIndex={activeTab} onTabChange={setActiveTab} />
-          <UnderConstruction />
-        </div>
+      <div className="flex flex-1 min-h-0">
+        <Tabs
+          tabPanelClassName="overflow-auto"
+          className="border-0"
+          tabs={TABS}
+          tabIndex={activeTab}
+          onTabChange={setActiveTab}
+        />
         <aside className="w-[352px] shrink-0">
           <AboutThisProject />
         </aside>
