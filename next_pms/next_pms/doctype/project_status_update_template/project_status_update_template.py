933ab025c97b5bb4aeec393922c3652bae5d99b9
robinroy03 <robinroy.work@gmail.com>
2026-05-08 13:30:32 +0530

    feat: enable python type annotations in hooks.py

--- next_pms/next_pms/doctype/project_status_update_template/project_status_update_template.py
+++ next_pms/next_pms/doctype/project_status_update_template/project_status_update_template.py
@@ -6,4 +6,17 @@ from frappe.model.document import Document
 
 
 class ProjectStatusUpdateTemplate(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        description: DF.TextEditor
+        template_name: DF.Data
+        title: DF.Data | None
+    # end: auto-generated types
+
     pass
