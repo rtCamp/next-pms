89cf039a59f6e07c2d9928942dbf7d83ff4b327c
Robin Roy <robinroy.work@gmail.com>
2026-05-21 15:35:21 +0530

    feat: comment thread implementation (#1318)
    * feat: comment thread implementation
    
    * chore: remove the old backwards compatible endpoint, rename the save_* endpoint to create_*
    
    * fix: make the reply_to field read only

--- next_pms/next_pms/doctype/project_comments/project_comments.py
+++ next_pms/next_pms/doctype/project_comments/project_comments.py
@@ -6,4 +6,22 @@ from frappe.model.document import Document
 
 
 class ProjectComments(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        comment: DF.TextEditor | None
+        created_at: DF.Datetime | None
+        modified_at: DF.Datetime | None
+        parent: DF.Data
+        parentfield: DF.Data
+        parenttype: DF.Data
+        reply_to: DF.Link | None
+        user: DF.Link | None
+    # end: auto-generated types
+
     pass
