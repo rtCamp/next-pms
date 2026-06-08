7002ce79b326b80503d82fa8e12f4f06e177f1a7
robinroy03 <robinroy.work@gmail.com>
2026-06-09 00:42:38 +0530

    fix: add created by and deletion lock

--- next_pms/next_pms/doctype/risk/risk.py
+++ next_pms/next_pms/doctype/risk/risk.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2026, rtCamp and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -26,7 +26,31 @@ class Risk(Document):
     # end: auto-generated types
 
     def before_save(self):
+        self._set_updated_by_on_new_rows()
+        self._prevent_deleting_others_rows()
         self._sync_fields_from_latest_update()
+
+    def _set_updated_by_on_new_rows(self):
+        for row in self.risk_update_log:
+            if row.is_new():
+                row.updated_by = frappe.session.user
+
+    def _prevent_deleting_others_rows(self):
+        if "System Manager" in frappe.get_roles():
+            return
+
+        submitted_names = {row.name for row in self.risk_update_log if row.name}
+        existing_rows = frappe.get_all(
+            "Risk Update",
+            filters={"parent": self.name},
+            fields=["name", "owner"],
+        )
+        for row in existing_rows:
+            if row["name"] not in submitted_names and row["owner"] != frappe.session.user:
+                frappe.throw(
+                    f"You can only delete rows you created. Row created by {row['owner']} cannot be removed.",
+                    frappe.PermissionError,
+                )
 
     def _sync_fields_from_latest_update(self):
         if not self.risk_update_log:
