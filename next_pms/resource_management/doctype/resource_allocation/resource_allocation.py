cb84a3c9fed989f0d0d0785f2e9bb529d2159d62
robinroy03 <robinroy.work@gmail.com>
2026-06-26 01:53:00 +0530

    fix: stale redis cache

--- next_pms/resource_management/doctype/resource_allocation/resource_allocation.py
+++ next_pms/resource_management/doctype/resource_allocation/resource_allocation.py
@@ -7,8 +7,8 @@ from frappe.utils import flt, today
 from frappe.utils.background_jobs import is_job_enqueued
 
 from next_pms.resource_management.api.project import (
+    _get_employees_resrouce_data_for_given_project,
     _get_resource_management_project_view_data,
-    get_employees_resrouce_data_for_given_project,
 )
 from next_pms.resource_management.api.team import _get_resource_management_team_view_data
 
@@ -92,7 +92,7 @@ def clear_cache(doc=None, method=None):
     def clear_cache_job():
         _get_resource_management_team_view_data.clear_cache()
         _get_resource_management_project_view_data.clear_cache()
-        get_employees_resrouce_data_for_given_project.clear_cache()
+        _get_employees_resrouce_data_for_given_project.clear_cache()
 
     job_name = "resource_allocation_clear_cache"
 
