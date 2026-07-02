7208b5d3f9e4626861c1ceea6160f675dbf5bc69
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2026-07-02 12:21:45 +0530

    fix: add tooltip on event chip
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- frontend/packages/design-system/src/components/calendar-timeline/eventChip.tsx
+++ frontend/packages/design-system/src/components/calendar-timeline/eventChip.tsx
@@ -1,6 +1,7 @@
 /**
  * Internal dependencies.
  */
+import { Tooltip } from "@rtcamp/frappe-ui-react";
 import {
   eventChipSubtitleVariants,
   eventChipTitleVariants,
@@ -15,12 +16,14 @@ interface EventChipProps {
 }
 
 const EventChip = ({ title, subtitle, color = "blue" }: EventChipProps) => (
-  <div className={eventChipVariants({ color })}>
-    <p className={eventChipTitleVariants({ color })}>{title}</p>
-    {subtitle && (
-      <p className={eventChipSubtitleVariants({ color })}>{subtitle}</p>
-    )}
-  </div>
+  <Tooltip text={title + "-" + subtitle}>
+    <div className={eventChipVariants({ color })}>
+      <p className={eventChipTitleVariants({ color })}>{title}</p>
+      {subtitle && (
+        <p className={eventChipSubtitleVariants({ color })}>{subtitle}</p>
+      )}
+    </div>
+  </Tooltip>
 );
 
 export default EventChip;
