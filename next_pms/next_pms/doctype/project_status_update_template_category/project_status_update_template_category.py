c97f5de1e7e3f18de842739be5ec828be295a040
robinroy03 <robinroy.work@gmail.com>
2026-08-06 17:09:14 +0530

    fix: perms and ci

--- next_pms/next_pms/doctype/project_status_update_template_category/project_status_update_template_category.py
+++ next_pms/next_pms/doctype/project_status_update_template_category/project_status_update_template_category.py
@@ -6,15 +6,14 @@ from frappe.model.document import Document
 
 
 class ProjectStatusUpdateTemplateCategory(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+    from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+    if TYPE_CHECKING:
+        from frappe.types import DF
 
+    # end: auto-generated types
 
-	# end: auto-generated types
-
-	pass
+    pass
