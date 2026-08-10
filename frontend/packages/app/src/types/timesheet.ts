bb8f63aac2e52cb81e627994a9f31997e98e1d5a
saranshsinhaa <saranshsinha084@gmail.com>
2026-08-10 16:08:40 +0530

    fix: remove unused type

--- frontend/packages/app/src/types/timesheet.ts
+++ frontend/packages/app/src/types/timesheet.ts
@@ -39,7 +39,6 @@ export interface TaskDataItemProps {
   project_name?: string | null;
   custom_approval_status?: ApprovalStatusLabelType | null;
   custom_rejection_reason?: string | null;
-  custom_weekly_rejection_reason?: string | null;
 }
 
 export interface LeaveProps {
