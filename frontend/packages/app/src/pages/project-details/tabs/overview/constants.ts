9dcd47af98d5b19ff315a875a6cfa90caa7c4afe
wreckage0907 <girish.raghav2004@gmail.com>
2026-08-26 13:36:03 +0530

    fix(overview): hide project fields that do not exist on the site

---
+++ frontend/packages/app/src/pages/project-details/tabs/overview/constants.ts
@@ -0,0 +1,11 @@
+export const SOURCING_FIELDS = [
+  "custom_source",
+  "custom_territory",
+  "custom_previous_cms",
+] as const;
+
+export const MARKETING_FIELDS = [
+  "custom_restricted_under_nda",
+  "custom_permission_for_case_study",
+  "custom_permission_for_testimonial",
+] as const;
