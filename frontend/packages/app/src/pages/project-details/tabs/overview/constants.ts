3ca7b5b302f60895dd2d867fe81fb087cdcd6b41
wreckage0907 <girish.raghav2004@gmail.com>
2026-08-26 16:06:07 +0530

    fix(overview): remove unused MARKETING_FIELDS export

--- frontend/packages/app/src/pages/project-details/tabs/overview/constants.ts
+++ frontend/packages/app/src/pages/project-details/tabs/overview/constants.ts
@@ -3,9 +3,3 @@ export const SOURCING_FIELDS = [
   "custom_territory",
   "custom_previous_cms",
 ] as const;
-
-export const MARKETING_FIELDS = [
-  "custom_restricted_under_nda",
-  "custom_permission_for_case_study",
-  "custom_permission_for_testimonial",
-] as const;
