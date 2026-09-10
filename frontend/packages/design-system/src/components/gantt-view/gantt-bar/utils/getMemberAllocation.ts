860a8c04c0057614e40dc58f1f9b417913ec88a6
Aditya Dhade <aditya.dhade@rtcamp.com>
2026-09-10 13:34:28 +0530

    chore: fix lint

--- frontend/packages/design-system/src/components/gantt-view/gantt-bar/utils/getMemberAllocation.ts
+++ frontend/packages/design-system/src/components/gantt-view/gantt-bar/utils/getMemberAllocation.ts
@@ -92,8 +92,7 @@ export function getAllocationSummary(
       billable: !dayHasNonBillable.get(ts),
       tentative: Boolean(dayHasTentative.get(ts)),
       type: (dayTimeoff.has(ts) ? "timeoff" : "default") as
-        | "default"
-        | "timeoff",
+        "default" | "timeoff",
       timeoff: dayTimeoff.get(ts),
       label: dayLabel.get(ts),
     }));
